d22a4348ddb17aa4b69dd8de34b823462cc91cf7
Ibrahim <ibrahimhosen4045@gmail.com>
2026-05-20 16:18:09 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
+
+
+const uri = process.env.MONGODB_URI;
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const db = client.db("Madiqueue")
+    const destinationCollection = db.collection("destinations")
+
+    app.post('/destination', async (req, res) => {
+        const destinationData = req.body
+        console.log(destinationData)
+        const result = await destinationCollection.insertOne(destinationData)
+        res.json(result)
+
+    })
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Server is running fine!")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
