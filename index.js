1bfee01b4765d3790c66bd1a242fcd892286ba85
Ibrahim <ibrahimhosen4045@gmail.com>
2026-05-21 14:46:17 +0600

    details section api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 
 
@@ -34,7 +34,11 @@ async function run() {
       res.json(result)
     })
 
-    
+    app.get("/destination/:id", async (req, res) => {
+      const {id} = req.params
+      const result = await destinationCollection.findOne({_id: new ObjectId(id)})
+      res.json(result)
+    })
 
     app.post('/destination', async (req, res) => {
         const destinationData = req.body
